a1a3a3d70a7ecd0e1aa501558a6aea71e88f5cfe
Parthiban <parthiban.rajendran238@gmail.com>
2024-03-05 09:23:54 +0530

    initial commit with project base files

---
+++ src/main/java/org/example/springproductcatalogue/SpringProductCatalogueApplication.java
@@ -0,0 +1,13 @@
+package org.example.springproductcatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductCatalogueApplication.class, args);
+	}
+
+}
