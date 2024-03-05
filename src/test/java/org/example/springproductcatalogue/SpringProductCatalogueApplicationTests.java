8e966ddf764c143279b62bac8d53ade88770d329
Parthiban <parthiban.rajendran238@gmail.com>
2024-03-05 09:23:54 +0530

    initial commit with project base files

---
+++ src/test/java/org/example/springproductcatalogue/SpringProductCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springproductcatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProductCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
